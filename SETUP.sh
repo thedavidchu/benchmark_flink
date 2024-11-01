aef2799ea32b769300ec0ab22d1c360e2f3481cb
David Chu <thedavidchu@gmail.com>
2024-11-01 19:13:48 +0000

    Remove broken or unnecessary SETUP

--- SETUP.sh
+++ SETUP.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
 ### @brief	Setup the environment to start developing (compile,run,etc) Apache Flink.
 
-# Activate Docker
-sudo docker run flink:latest jobmanager
+# Get the path for the 'benchmark_flink' directory
+DIR="$(git rev-parse --show-toplevel)"
 
-# Build Apache Flink (quickly)
+# Build Apache Flink
+cd "${DIR}/flink"
+# NOTE  The following fails if you have even a slightly different version of Maven!
+#mvn clean install -DskipTests
+# Or build it QUICKLY:
 mvn clean install -DskipTests -Dfast -Pskip-webui-build -T 1C
 
+# Build Apache PyFlink
+# NOTE  The following fails because of some errors. Ugh.
+#python3.11 -m pip install -r flink-python/dev/dev-requirements.txt
